ca877f9c8d1debfeef6ee44f2ab345707dceb934
ValdineiCS <109873014+ValdineiCS@users.noreply.github.com>
2025-11-05 14:12:51 -0300

    estrutura html criada

---
+++ script.js
@@ -0,0 +1,34 @@
+let form=document.getElementById('formViagem')
+
+let inOrigem=document.getElementById('inOrigem');
+let inDestino=document.getElementById('inDestino');
+let inDistancia=document.getElementById('inDistancia');
+let inValor=document.getElementById('inValor');
+let botao=document.getElementById('botSalvar')
+let msg=document.getElementById('msg')
+botao.addEventListener('click', salvarViagem=>{
+    salvarViagem.preventDefault();
+
+    const origem=inOrigem.value;
+    const destino=inDestino.valu;
+    const distancia=Number(inDistancia.value);
+    const valor=Number(inValor.value);
+
+    if(!origem || !destino || distancia<=0 || valor<=0){
+        msg.textContent='Preencha todos os campos corretamente.';
+        msg.style.color='red';
+        return;
+    }
+
+    const viagem={ origem, destino, distancia, valor};
+
+    const viagens=JSON.parse(localStorage.getItem('viagens'))|| [];
+    viagens.push(viagem);
+
+    localStorage.setItem('viagens', JSON.stringify(viagens));
+    msg.textContent="Viagem salva com sucesso!"
+    msg.style.color='lime';
+
+    form.reset();
+
+});
